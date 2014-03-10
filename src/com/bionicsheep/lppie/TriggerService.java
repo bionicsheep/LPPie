d704ddf193cdedbbe665fed25f7b3baa321366e3
bionicsheep <alessandroeppacher@gmail.com>
2014-03-10 14:42:23 -0400

    Fading to dark on UiThread
    WOOOOOOOPDIE FUCKIN DOOOOOOOOOOOOOOOOOOOO

--- src/com/bionicsheep/lppie/TriggerService.java
+++ src/com/bionicsheep/lppie/TriggerService.java
@@ -2,8 +2,11 @@ package com.bionicsheep.lppie;
 
 import android.app.Service;
 import android.content.Intent;
+import android.graphics.Color;
 import android.graphics.PixelFormat;
+import android.os.Handler;
 import android.os.IBinder;
+import android.util.Log;
 import android.view.Display;
 import android.view.Gravity;
 import android.view.MotionEvent;
@@ -15,13 +18,21 @@ import android.widget.Toast;
 
 public class TriggerService extends Service{
 
-	ImageView detectorArea;
+	ImageView detectorArea, background, pieOutline, temp;
 	WindowManager wm;
-	WindowManager.LayoutParams params;
-	int dwidth;
-	int dheight;
+	WindowManager.LayoutParams tparams, bparams, pparams;
+	int twidth, theight;
+	int bwidth, bheight;
+	int pwidth, pheight;
 	Toast toast;
+	boolean active = false;
+	boolean scanning = true;
 	Service currentActivity = this;
+	int dragY;
+	int shadow_threshold;
+	Display display;
+
+	Handler handler;
 
 	@Override
 	public int onStartCommand(Intent intent, int flags, int startId) {
@@ -30,30 +41,18 @@ public class TriggerService extends Service{
 		return START_STICKY;
 	}
 
-	@SuppressWarnings("deprecation")
 	@Override
 	public void onCreate(){
 		detectorArea = new ImageView(this);
-		detectorArea.setImageResource(R.drawable.detector);
-		detectorArea.setOnTouchListener(triggerTouchListener);
-		detectorArea.setScaleType(ImageView.ScaleType.FIT_XY);
+		background = new ImageView(this);
+		pieOutline = new ImageView(this);
+
+		handler = new Handler();
 
 		wm = (WindowManager) getSystemService(WINDOW_SERVICE);		
-		Display display = wm.getDefaultDisplay();
-		dwidth = display.getWidth() / 2;
-		dheight = 5;
+		display = wm.getDefaultDisplay();
 
-		params = new WindowManager.LayoutParams(
-				dwidth,
-				dheight,
-				WindowManager.LayoutParams.TYPE_PHONE,
-				WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE,
-				PixelFormat.TRANSLUCENT
-				);
-
-		params.gravity = Gravity.CENTER | Gravity.BOTTOM;
-
-		wm.addView(detectorArea, params);
+		startTrigger();
 	}
 
 	@Override
@@ -66,10 +65,23 @@ public class TriggerService extends Service{
 		@Override
 		public boolean onTouch(View v, MotionEvent event) {
 			if(event.getAction() == MotionEvent.ACTION_DOWN){
-				Intent i = new Intent(getBaseContext(), Pie.class);
-				i.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-				i.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
-				getApplication().startActivity(i);
+				active = true;
+				Log.d("pie:", "bound");
+				startBackground();
+				startPie();
+			}else if(event.getAction() == MotionEvent.ACTION_UP){
+				active = false;
+				wm.removeView(background);
+				wm.removeView(pieOutline);
+				scanning = true;
+				Log.d("pie:", "release");
+			}else if(event.getAction() == MotionEvent.ACTION_MOVE){
+				dragY = (int) -event.getY();
+				if(scanning && dragY > shadow_threshold){
+					Log.d("pie", "threshold hit");
+					scanning = false;
+					fadeToDark();
+				}
 			}
 			return true;
 		}
@@ -78,10 +90,98 @@ public class TriggerService extends Service{
 	@Override
 	public void onDestroy(){
 		if(detectorArea != null) { 
-			if(detectorArea != null){
-				wm.removeView(detectorArea);
-			}
+			wm.removeView(detectorArea);
 		}
 		super.onDestroy();
+	}
+
+	public void fadeToDark(){
+
+		(new Thread(){
+			int n = 0;
+			public void run(){
+				
+				for(n = 0; n < 200; n++){
+					try{
+						runOnUiThread(new Runnable(){
+							@Override
+							public void run() {
+								background.setBackgroundColor(Color.argb(n, 0, 0, 0));
+							}
+						});
+						sleep(3);
+					} catch (InterruptedException e) {
+						e.printStackTrace();
+					}
+				}
+			}
+
+		}).start();
+
+	}
+
+	@SuppressWarnings("deprecation")
+	public void startPie(){
+		pieOutline.setImageResource(R.drawable.pie);
+		pieOutline.setScaleType(ImageView.ScaleType.FIT_XY);
+
+		pwidth = display.getWidth();
+		pheight = pwidth / 2;
+
+		pparams = new WindowManager.LayoutParams(
+				pwidth,
+				pheight,
+				WindowManager.LayoutParams.TYPE_PHONE,
+				WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE,
+				PixelFormat.TRANSLUCENT
+				);
+
+		pparams.gravity = Gravity.CENTER | Gravity.BOTTOM;
+		wm.addView(pieOutline, pparams);
+	}
+
+	@SuppressWarnings("deprecation")
+	public void startTrigger(){
+		detectorArea.setImageResource(R.drawable.detector);
+		detectorArea.setOnTouchListener(triggerTouchListener);
+		detectorArea.setScaleType(ImageView.ScaleType.FIT_XY);
+		twidth = display.getWidth() / 2;
+		theight = 5;
+		shadow_threshold = display.getHeight() / 3;
+
+		tparams = new WindowManager.LayoutParams(
+				twidth,
+				theight,
+				WindowManager.LayoutParams.TYPE_PHONE,
+				WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE,
+				PixelFormat.TRANSLUCENT
+				);
+
+		tparams.gravity = Gravity.CENTER | Gravity.BOTTOM;
+
+		wm.addView(detectorArea, tparams);
+	}
+
+	@SuppressWarnings("deprecation")
+	public void startBackground(){
+		bwidth = display.getWidth();
+		bheight = display.getHeight();
+
+		bparams = new WindowManager.LayoutParams(
+				bwidth,
+				bheight,
+				WindowManager.LayoutParams.TYPE_PHONE,
+				WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE,
+				PixelFormat.TRANSPARENT
+				);
+
+		bparams.gravity = Gravity.CENTER | Gravity.BOTTOM;
+
+		wm.addView(background, bparams);
+		background.setBackgroundColor(Color.argb(0, 0, 0, 0));
+	}
+
+	private void runOnUiThread(Runnable runnable) {
+		handler.post(runnable);
 	}
 }
